439a16a614aaf31099adecaabdeac340e3190cb9
yusuf <arasy541@gmail.com>
2025-03-27 13:23:35 +0300

    bug fixed
    bug fixed

--- next.config.js
+++ next.config.js
@@ -1,28 +1,27 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Strict mode kapalı
+  reactStrictMode: process.env.NODE_ENV === 'development', // Sadece geliştirme modunda strict mode
   experimental: {
-    serverActions: true, // Server Actions özelliğini etkinleştir
+      serverActions: true,
   },
   images: {
-    domains: [
-      'images.unsplash.com',
-      'i.ibb.co',
-      'scontent.fotp8-1.fna.fbcdn.net',
-    ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.googleusercontent.com',
-        port: '',
-        pathname: '**',
-      },
-    ],
+      domains: [
+          'images.unsplash.com',
+          'i.ibb.co',
+          'scontent.fotp8-1.fna.fbcdn.net',
+      ],
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'lh3.googleusercontent.com',
+              pathname: '/a/**',
+          },
+          {
+              protocol: 'https',
+              hostname: '*.googleusercontent.com',
+              pathname: '**',
+          },
+      ],
   },
 };
 
