cc380de5750493ea2a25cb53871f1ebc583607b2
yusuf <arasy541@gmail.com>
2025-03-27 16:13:54 +0300

    yenileme
    yenileme

--- next.config.js
+++ next.config.js
@@ -1,27 +1,28 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: process.env.NODE_ENV === 'development', // Sadece geliştirme modunda strict mode
+  reactStrictMode: false, // Strict mode kapalı
   experimental: {
-      serverActions: true,
+    serverActions: true, // Server Actions özelliğini etkinleştir
   },
   images: {
-      domains: [
-          'images.unsplash.com',
-          'i.ibb.co',
-          'scontent.fotp8-1.fna.fbcdn.net',
-      ],
-      remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: 'lh3.googleusercontent.com',
-              pathname: '/a/**',
-          },
-          {
-              protocol: 'https',
-              hostname: '*.googleusercontent.com',
-              pathname: '**',
-          },
-      ],
+    domains: [
+      'images.unsplash.com',
+      'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
   },
 };
 
